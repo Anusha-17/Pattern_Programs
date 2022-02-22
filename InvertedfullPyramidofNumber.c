384a2c96213404e08ca8d90ef03ecfc542d71b35
Anusha-17 <98573933+Anusha-17@users.noreply.github.com>
2022-02-22 22:43:52 +0530

    Program to print the inverted full Pyramid of numbers

---
+++ InvertedfullPyramidofNumber.c
@@ -0,0 +1,25 @@
+#include <stdio.h>  
+int main()  
+{  
+    // declare the local variables  
+    int i, j, rows, k, m = 1;  
+    printf (" Enter a number to define the rows: \n");  
+    scanf ("%d", &rows); // take a number  
+    printf("\n");  
+    // outer loop define the total rows and i should be greater than equal to 1  
+    for ( i = rows; i >= 1; i--)  
+    {  
+        // inner loop define j should be less than equal to m  
+        for ( j = 1; j <= m; j++)  
+        {  
+            printf ("  ");   
+        }  
+        for ( k = 1; k <= ( 2 * i - 1); k++)  
+        {  
+            printf ("%d ", i); // print the number  
+        }  
+        m++;  
+        printf ("\n");  
+    }  
+    return 0;  
+}  
